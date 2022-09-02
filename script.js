553f85c5a0fd7af909ae679d6f0eaf25d7794dc7
Hilman Hidayat <99820483+kodeman274@users.noreply.github.com>
2022-09-02 23:14:36 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const galery = document.querySelector(".galery");
+const jumbo = document.querySelector(".jumbo");
+let eOld = null;
+
+galery.addEventListener("click", function (e) {
+  if (e.target.className == "thumb") {
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 500);
+  }
+});
+
+// const nav = document.querySelector(".nav");
+// nav.style.display = "none";
+
+const galery1 = document.querySelector(".galery1");
+const jumbo1 = document.querySelector(".jumbo1");
+
+galery1.addEventListener("click", function (e) {
+  if (e.target.className == "thumb1") {
+    jumbo1.src = e.target.src;
+    jumb1o.classList.add("fade");
+    setTimeout(function () {
+      jumbo1.classList.remove("fade");
+    }, 500);
+  }
+});
